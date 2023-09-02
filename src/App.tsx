1a7d463507b892c67f67996fbc114de5f1b06afb
Aviad Udi <aviadudi1@gmail.com>
2023-09-02 12:03:39 +0300

    my second commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CouponsContainer from "./components/CouponsContainer/CouponsContainer";
-import AsideMenu from "./components/AsideMenu/AsideMenu";
 import Layout from "./components/Layout/Layout";
 
 function App() {
+  console.log("started");
+  
   return (
     <div className="App">
       <Layout/>
